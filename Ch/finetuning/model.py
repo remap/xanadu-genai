beaf1a065772cf30a680b3e4f6dde68c2cad1942
Naisha Agarwalnaishaa@g.ucla.edu <naishaa@pluslab02-a6000.cs.ucla.edu>
2024-11-28 12:26:31 -0800

    inference for dreambooth

--- Ch/finetuning/model.py
+++ Ch/finetuning/model.py
@@ -1,2 +1,17 @@
 # finetuning stable diffusion model with vaporwave images
+# inference on calliope model 
+
+from diffusers import StableDiffusionPipeline
+
+model_path = "./calliope-model"
+pipe = StableDiffusionPipeline.from_pretrained(model_path)
+pipe = pipe.to("cuda")  # If using GPU
+
+prompt = "a photo of sks Calliope on a boat"
+image = pipe(prompt).images[0]
+#image.show()
+
+image.save("calliope_boat_image.png")  # Save as PNG
+print("Image saved as 'generated_image.png'")
+
 
